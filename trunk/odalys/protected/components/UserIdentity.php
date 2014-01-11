653d5f748f55f56e565c2059c00bbd2f904827f4
edgar.leal0@gmail.com <edgar.leal0@gmail.com@6d70ca8b-ca6a-631d-2838-4a0ed48bd04f>
2014-01-11 18:24:42 +0000

    limpieza url y autenticacion desde bd

--- trunk/odalys/protected/components/UserIdentity.php
+++ trunk/odalys/protected/components/UserIdentity.php
@@ -7,6 +7,7 @@
  */
 class UserIdentity extends CUserIdentity
 {
+	private $_id;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -17,17 +18,35 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
+		/*$users=array(
 			// username => password
 			'demo'=>'demo',
 			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+		);*/
+		if(isset($this->username))
+		{
+			 $username=strtolower($this->username);
+			$user=Usuarios::model()->find('LOWER(email)=?',array($username));
+			
+			if($user===null)
+				$this->errorCode=self::ERROR_USERNAME_INVALID;
+			else if(!$user->validatePassword($this->password))
+				$this->errorCode=self::ERROR_PASSWORD_INVALID;
+			else{
+			 	$this->_id=$user->id;
+			 	$this->username=$user->email;
+			 	$this->errorCode=self::ERROR_NONE;
+			 	Yii::app()->session['id_usuario'] = $user->id;
+			 	Yii::app()->session['nombre_usuario'] = $user->nombre;
+			 	Yii::app()->session['apellido_usuario'] = $user->apellido;
+			}
+				 return $this->errorCode==self::ERROR_NONE;
+		}
+		
+	}
+
+	public function getId()
+ 	{
+		 return $this->_id;
 	}
 }
