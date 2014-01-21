266a1117ecdccfd10bb4b2ec48155f20132b23b8
edgar.leal0@gmail.com <edgar.leal0@gmail.com@6d70ca8b-ca6a-631d-2838-4a0ed48bd04f>
2014-01-21 17:45:25 +0000

    resultados vista

---
+++ trunk/odalys/protected/views/site/resultados.php
@@ -0,0 +1,8 @@
+<?php //Yii::app()->clientScript->registerCoreScript('jquery');
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+
+echo $resultados;
+//print_r($resultados);
+?>
