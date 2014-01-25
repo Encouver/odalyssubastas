34c2ee847518f43273bfab084eb4b717e598f301
eal0 <edgar.leal0@gmail.com>
2014-01-25 22:54:22 +0000

    envio de email

--- odalys/protected/controllers/MailController.php
+++ odalys/protected/controllers/MailController.php
@@ -37,116 +37,167 @@ class MailController extends Controller
 
 	public function actionCompradores()
 	{
+		$arreglo = array();
 
-		$silenciosa = Subastas::model()->find('silenciosa=:silenciosa', array(':silenciosa'=>1));
-		
+		$criteria = new CDbCriteria;
 
-		$ganadores = ImagenS::model()->findAll('ids=:ids', array(':ids' => $silenciosa['id']));
+		$criteria->condition = 'fuesilenciosa=:fuesilenciosa';
+		$criteria->params = array(':fuesilenciosa'=>1);
+		$criteria->order = 'id DESC';
 
-		
-		foreach ($ganadores as $key => $value) {
+		$silenciosa = Subastas::model()->find($criteria);		
 			
+		$subject = 'Resultados de la '.$silenciosa['nombre'].' '.$silenciosa['nombrec'];
 
+		$usuarios = ImagenS::model()->findAll('ids=:ids', array(':ids' => $silenciosa['id']));
 
-			if($value->id_usuario)
+		
+		foreach ($usuarios as $key => $value)
+		{
+			
+			if($value->id_usuario and !in_array($value->id_usuario, $arreglo))
 			{
 
-					echo $value->id_usuario.'<br/>';
+				$usuario = Usuarios::model()->find('id=:id', array(':id'=>$value->id_usuario));
 
-			}
-		}
+				$usuariospuja = Usuariospujas::model()->find('idusuario=:idusuario', array(':idusuario'=>$usuario['id']))
+				//Metodo hacer invocado cada vez que un pujador pierda en la misma.
 
-		//echo $silenciosa['id'];
+				$correo = $usuario['email'];
+				$nombre = $usuario['nombre'];
+				$apellido = $usuario['apellido'];
 
-		//Falta consultar los compradores de las obras es decir la tabla imagen_s y hacer un foreach
-		/*
-		$correo = "edgar.leal0@gmail.com";
-		$nombre = "edgar";
-		$apellido = "apellido";
+				$paleta = $usuariospuja['paleta'];
 
-		
-		$to      = $correo;
-		$subject = 'Registro satisfactorio en www.odalys.com';
-		$message = 
+				$to = $correo;
 
-		'
-		<html>
-			<head>
-  				<title>Grupo Odalys</title>
-			</head>
-				<body>
-					¡Bienvenido(a) '.$nombre.' '.$apellido.'! <br/>Su registro se ha realizado satisfactoriamente satisfactoriamente en www.odalys.com<br><br/>
-					Al acceder a su cuenta en <a href="http://www.odalys.com/odalys/inicio.php">www.odalys.com</a> usted podr&aacute;:
+				$message = '
+		 <div style="padding-left:100px !important; padding-top:10px !important; float:left !important; padding-right:50px !important;">
+               <h2 style="padding-bottom:10px !important; font-size:14px !important;">Estimado cliente, la '.$silenciosa['nombre'].' '.$silenciosa['nombrec'].' (Subasta Silenciosa) ha finalizado.</h2>   
+				<table class="table">
+				  <thead>
+				    <tr>
+				      <th>Nombre</th>
+				      <th>Apellido</th>
+				      <th>Paleta</th>
+				      <th>Referencia</th>
+				      <th>Monto</th>
+				      <th>Lote</th>
+				    </tr>
+				  </thead>
+				  <tbody>';
 
-					<li>Descargar los resultados de las &uacute;ltimas subastas.</li>
-					<li>Dejar pujas en ausencia o telef&oacute;nicas para nuestras subastas.</li>
-					<li>Participar en las Subastas Silenciosas.</li>
-					<li>Recibir comunicaciones del Grupo Odalys.</li>
-					<li>Pr&oacute;ximamente, comprar en nuestra tienda virtual y subastas online.</li>
-					
-					</body>
-		</html>
-		';
+				  $arreglo[] = $value->id_usuario;
 
-	 
-		 	$headers  = 'MIME-Version: 1.0' ."\r\n";
+				  $usuarios = ImagenS::model()->findAll('ids=:id_usuario', array(':id_usuario' => $value->id_usuario));
+				 
+				  foreach ($usuarios as $ky => $valor) {
+				  		
+				  	$message .=
+				  		'
+						 "<tr>";
+						 "<td>";        
+					       '.$nombre.'
+					 "</td>";
+					"<td>";
+					       '.$apellido.'
+					 "</td>";
+					 "<td>";
+					       '.$paleta.'
+					 "</td>";
+
+					 "<td>";
+					        '.$valor->descri.'
+					 "</td>"';
+
+					 $monto18 = $valor->actual*1.18;
+
+					 $iva = $monto18*1.12;
+
+					 $total = $monto18 + $iva + $valor->actual;
+
+					 '"<td>";
+					  echo "Bs. '.$total.'";
+					 "</td>";
+					 "<td>";
+					  echo "<img src=http://odalys.com/odalys/"'.$valor->imagen.'">";
+					"</td>";
+
+					"</tr>"';
+
+				  }
+				
+					$message .=  '</tbody>
+				</table>
+				<hr>
+					Recuerde que las condiciones de la subasta establecen que tiene 7 días para cancelar sus piezas en los espacios de la Casa de Subastas en el Centro Comercial Concresa, PB en el horario de Martes a Sábado de 10:00 a.m. a 6:00 p.m.<br/>
+
+			Saludos,
+
+			Casa de Subastas Odalys
+			C. Comercial Concresa, Nivel PB. Local 
+			115 y 116, Prados del Este, Baruta 1080,
+			Estado Miranda, Venezuela
+			Telfs: +58 2129795942, +58 2129761773
+			Fax: +58 212 9794068
+			odalys@odalys.com
+    	</div> ';
+
+
+    		$headers  = 'MIME-Version: 1.0' ."\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 			$headers .= 'From: Grupo Odalys<galeriaccs@odalys.com>'. "\r\n" .
 		    'Reply-To: pujas@odalys.com' . "\r\n" .
 		    'X-Mailer: PHP/' . phpversion();
 
 
-
-
-		    if (mail($to, $subject, $message, $headers)) {
-		     	$this->layout='//layouts/column1';
-		    	$valor = true;
-				$this->render('index', array('valor'=>$valor));
-		    } else {
+		       if (mail($to, $subject, $message, $headers)) {
+			     	$this->layout='//layouts/column1';
+			    	$valor = true;
+					$this->render('index', array('valor'=>$valor));
+		    	} else {
 
 			    	$this->layout='//layouts/column1';
 			    	$valor = false;
 					$this->render('index', array('valor'=>$valor));
 		    	
-		    }*/
+		   		 }
 
 
+			}
+		}
 	}
 
-	public function actionPujadores()
+	public function actionPujadores($id, $descri)
 	{
 
+		$silenciosa = Subastas::model()->find('silenciosa=:silenciosa', array(':silenciosa'=>1));
+
+		$usuario = Usuarios::model()->find('id=:id', array(':id'=>$id));
 		//Metodo hacer invocado cada vez que un pujador pierda en la misma.
 
-		$correo = "edgar.leal0@gmail.com";
-		$nombre = "edgar";
-		$apellido = "apellido";
+		$correo = $usuario['email'];
+		$nombre = $usuario['nombre'];
+		$apellido = $usuario['apellido'];
 
 		
 		$to      = $correo;
-		$subject = 'Registro satisfactorio en www.odalys.com';
+		$subject = 'Puja superada en la '.$silenciosa['nombre'].' '.$silenciosa['nombrec'];
 		$message = 
 
 		'
 		<html>
 			<head>
-  				<title>Grupo Odalys</title>
+  				<title>'.$silenciosa['nombre'].' '.$silenciosa['nombrec'].' Casa de Subastas Odalys</title>
 			</head>
 				<body>
-					¡Bienvenido(a) '.$nombre.' '.$apellido.'! <br/>Su registro se ha realizado satisfactoriamente satisfactoriamente en www.odalys.com<br><br/>
-					Al acceder a su cuenta en <a href="http://www.odalys.com/odalys/inicio.php">www.odalys.com</a> usted podr&aacute;:
+					Estimado(a) '.$nombre.' '.$apellido.'<br><br>Ha sido superada la puja que realizÃ³ por el lote:<br> '.$descri.'<br/>
+					<br>Si desea puede hacer una puja por esta obra nuevamente. <a href="http://subastas.odalys.com/">Ir a la '.$silenciosa['nombre'].' '.$silenciosa['nombrec'].'</a>
+					<br/>
 
-					<li>Descargar los resultados de las &uacute;ltimas subastas.</li>
-					<li>Dejar pujas en ausencia o telef&oacute;nicas para nuestras subastas.</li>
-					<li>Participar en las Subastas Silenciosas.</li>
-					<li>Recibir comunicaciones del Grupo Odalys.</li>
-					<li>Pr&oacute;ximamente, comprar en nuestra tienda virtual y subastas online.</li>
-					
-					</body>
+				</body>
 		</html>
 		';
-
-	 
 	 	$headers  = 'MIME-Version: 1.0' ."\r\n";
 		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 		$headers .= 'From: Grupo Odalys<galeriaccs@odalys.com>'. "\r\n" .
