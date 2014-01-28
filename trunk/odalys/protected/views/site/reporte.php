faf8e1567ddf1a97f76392991d1516e536c46030
edgar.leal0@gmail.com <edgar.leal0@gmail.com@6d70ca8b-ca6a-631d-2838-4a0ed48bd04f>
2014-01-28 16:18:37 +0000

    reporte integrado.

---
+++ trunk/odalys/protected/views/site/reporte.php
@@ -0,0 +1,16 @@
+<?php
+	$subasta = 1;
+	$content = "
+<<html>
+<head>
+	<title>Probando</title>
+</head>
+<body>
+    <h1>Repore subasta ".$subasta."</h1>
+    <br>
+    
+</body>
+</html>";
+
+ 	echo $content;
+?>
